28861b0cf5c4a08dcd19ef873f1d0fc021ac801f
Vinayak-2108 <vinayakchatterjee28@gmail.com>
2022-07-08 14:43:24 +0530

    navbar setup, initialised home page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Blog from "./components/Blog";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Home = () => {
+    return ( 
+        <>
+            <div className="body">
+                <div className="text"></div>
+                <div className="image"></div>
+            </div>
+            <h1 className='hero-text'>Market Forcast</h1>
+        </>
+     );
+}
+ 
+export default Home;
